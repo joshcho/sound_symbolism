a86489dec326220aa5cb0a70534dd44d4ef42a19
Josh Cho <joshchonpc@gmail.com>
2020-10-23 03:44:25 +0900

    Implement more compact way to save matrices
    It now saves matrices before shuffling.

--- g2p.py
+++ g2p.py
@@ -2,6 +2,8 @@ from g2p_en import G2p
 import numpy as np
 import util
 import naivebayes
+import os.path
+from os import path
 # for bad_words
 import nltk
 from nltk.corpus import words
@@ -39,6 +41,7 @@ def transform_text(texts, phoneme_dict):
     return np.array(matrix)
 
 def split_words(all_words):
+    random.seed(1234)
     train_words = \
         random.sample(all_words, len(all_words) *2 // 3)
     diff_words = list(set(all_words).difference(set(train_words)))
@@ -46,40 +49,54 @@ def split_words(all_words):
     test_words = list(set(diff_words).difference(set(val_words)))
     return train_words, val_words, test_words
 
+def transform_words_and_save(word_set, save_path):
+    matrix = transform_text(word_set, phoneme_dict)
+    np.savetxt(save_path, matrix)
 
 def train_bad_words():
+    nltk_matrix_path = "nltk_words_matrix.gz"
+    bad_matrix_path = "fb_bad_words_matrix.gz"
     nltk.download('words')
-    random.seed(1234)
+    nltk_words = words.words()
+
+    # Note that some words in bad_words may not be in nltk_words
     bad_words = np.loadtxt("facebook-bad-words.txt",delimiter=',',dtype=str)
-    print(len(bad_words))
-    # some bad words are not in nltk corpus
-    all_words = list(set(words.words()).union(bad_words))
-    train_words, val_words, test_words = split_words(all_words)
-    train_labels = np.array([1 if word in bad_words else 0 \
-                             for word in train_words])
-    val_labels = np.array([1 if word in bad_words else 0 \
-                             for word in val_words])
-    test_labels = np.array([1 if word in bad_words else 0 \
-                             for word in test_words])
 
-    train_matrix = transform_text(train_words, phoneme_dict)
-    np.savetxt("bad_words_train_matrix.gz", train_matrix)
-    val_matrix = transform_text(val_words, phoneme_dict)
-    np.savetxt("bad_words_val_matrix.gz", val_matrix)
-    test_matrix = transform_text(test_words, phoneme_dict)
-    np.savetxt("bad_words_test_matrix.gz", test_matrix)
+    if not path.exists(nltk_matrix_path):
+        transform_words_and_save(nltk_words, nltk_matrix_path)
 
-    # train_matrix = np.loadtxt("bad_words_train_matrix.gz")
-    # test_matrix = np.loadtxt("bad_words_test_matrix.gz")
+    if not path.exists(bad_matrix_path):
+        transform_words_and_save(bad_words, bad_matrix_path)
+
+    nltk_matrix = np.loadtxt(nltk_matrix_path)
+    bad_matrix = np.loadtxt(bad_matrix_path)
+
+    nltk_labels = np.array([[1 if word in bad_words else 0 \
+                             for word in nltk_words]]).T
+    bad_labels = np.ones((len(bad_words), 1))
+
+    # shuffling both matrix and labels together and merging nltk and bad
+    nltk_dummy = np.concatenate((nltk_matrix, nltk_labels), axis=1)
+    bad_dummy = np.concatenate((bad_matrix, bad_labels), axis=1)
+    dummy = np.concatenate((nltk_dummy, bad_dummy), axis=0)
+    np.random.shuffle(dummy)
+    matrix, labels = np.split(dummy, [-1], axis=1)
+    labels = np.reshape(labels, (len(labels),))
+
     model = {}
-    model["train_matrix"] = train_matrix
-    model["val_matrix"] = val_matrix
-    model["test_matrix"] = test_matrix
-    model["train_labels"] = train_labels
-    model["val_labels"] = val_labels
-    model["test_labels"] = test_labels
+    n = len(matrix)
+    index1 = n // 3
+    index2 = n * 2 // 3
+    model["train_matrix"] = matrix[0: index1]
+    model["train_labels"] = labels[0: index1]
+    model["val_matrix"] = matrix[index1: index2]
+    model["val_labels"] = labels[index1: index2]
+    model["test_matrix"] = matrix[index2:]
+    model["test_labels"] = labels[index2:]
 
     naivebayes.run_naive_bayes(model, phoneme_dict, "bad_words_predictions")
+
+train_bad_words()
 
 def train_imdb():
     train_texts, train_labels = \
