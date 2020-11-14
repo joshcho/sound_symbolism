f391e291ac2de6f7d87283ddf2efd4d89bd731ae
Josh Cho <joshchonpc@gmail.com>
2020-11-14 13:36:17 +0900

    Refactor g2p.py

--- g2p.py
+++ g2p.py
@@ -11,14 +11,10 @@ from nltk.corpus import words
 import random
 from sklearn.utils import shuffle
 
-"""
-Feature List
-1. Save phonemes, not encodings. We may use the phonemes later for RNN.
-2. If we need to do a lot of conversions, then directly using cmudict in nltk.corpus may be better than using g2p. g2p predicts pronunciation for words that do not exist, which may take a lot of time. For something like YouTube channel names, however, g2p is very useful.
-"""
-
 g2p = G2p()
 phoneme_dict = util.get_phoneme_dict()
+nltk_matrix_path = "nltk_words_matrix.gz"
+bad_matrix_path = "fb_bad_words_matrix.gz"
 
 def transform_text(texts, phoneme_dict):
     """
@@ -42,73 +38,33 @@ def transform_text(texts, phoneme_dict):
         index += 1
     return np.array(matrix)
 
-def split_words(all_words):
-    random.seed(1234)
-    train_words = \
-        random.sample(all_words, len(all_words) *2 // 3)
-    diff_words = list(set(all_words).difference(set(train_words)))
-    val_words = random.sample(diff_words, len(diff_words) // 2)
-    test_words = list(set(diff_words).difference(set(val_words)))
-    return train_words, val_words, test_words
-
 def transform_words_and_save(word_set, save_path):
     matrix = transform_text(word_set, phoneme_dict)
     np.savetxt(save_path, matrix)
 
 def train_bad_words():
-    nltk_matrix_path = "nltk_words_matrix.gz"
-    bad_matrix_path = "fb_bad_words_matrix.gz"
     nltk.download('words')
     nltk_words = words.words()
 
     # Note that some words in bad_words may not be in nltk_words
     bad_words = np.loadtxt("facebook-bad-words.txt",delimiter=',',dtype=str)
 
-    if not path.exists(nltk_matrix_path):
+    # Load if already saved
+    if path.exists(nltk_matrix_path):
+        nltk_matrix = np.loadtxt(nltk_matrix_path)
+    else:
         transform_words_and_save(nltk_words, nltk_matrix_path)
 
-    if not path.exists(bad_matrix_path):
+    if path.exists(bad_matrix_path):
+        bad_matrix = np.loadtxt(bad_matrix_path)
+    else:
         transform_words_and_save(bad_words, bad_matrix_path)
-
-    nltk_matrix = np.loadtxt(nltk_matrix_path)
-    bad_matrix = np.loadtxt(bad_matrix_path)
-
-    if True: # analyze frequency of words in each dataset
-        nltk_counts = np.sum(nltk_matrix,axis=0)
-        bad_counts = np.sum(bad_matrix,axis=0)
-        freq = nltk_counts/np.sum(nltk_counts)
-        nltk_freq_dict = phoneme_dict.copy()
-        for phoneme, i in nltk_freq_dict.items():
-            nltk_freq_dict[phoneme] = freq[i]
-
-        freq = bad_counts/np.sum(bad_counts)
-        freq_dict = phoneme_dict.copy()
-        for phoneme, i in freq_dict.items():
-            freq_dict[phoneme] = freq[i]
-
-        bad_list = []
-        for phoneme in sorted(freq_dict, key=freq_dict.get,reverse=True):
-            bad_list.append([phoneme, freq_dict[phoneme]])
-
-        plt.figure(figsize=(15,10))
-        plt.title("bad words (y = 1)")
-        for i in range(len(bad_list)//2):
-            plt.bar(bad_list[i][0], float(bad_list[i][1]), width=0.8,color='r',linewidth=3)
-        plt.xlabel('phonemes')
-        plt.ylabel('frequency in %')
-        plt.savefig('plot_bad.png')
-        plt.clf()
-
-        plt.title("safe words (y = 0)")
-        for i in range(len(bad_list)//2):
-            phoneme = bad_list[i][0]
-            plt.bar(phoneme, float(nltk_freq_dict[phoneme]), width=0.8,color='b',linewidth=1.5)
-        plt.savefig('plot_all.png')
 
     nltk_labels = np.array([[1 if word in bad_words else 0 \
                              for word in nltk_words]]).T
     bad_labels = np.ones((len(bad_words), 1))
 
+    # shuffle matrix
     matrix = np.concatenate((nltk_matrix, bad_matrix), axis=0)
     labels = np.concatenate((nltk_labels, bad_labels), axis=0)
     matrix, labels = shuffle(matrix, labels, random_state=0)
@@ -130,24 +86,30 @@ def train_bad_words():
 train_bad_words()
 
 def train_imdb():
-    train_texts, train_labels = \
-        util.load_csv('imdb_train.csv')
-    val_texts, val_labels = \
-        util.load_csv('imdb_valid.csv')
-    test_texts, test_labels = \
-        util.load_csv('imdb_test.csv')
+    train_texts, train_labels = util.load_csv('imdb_train.csv')
+    val_texts, val_labels = util.load_csv('imdb_valid.csv')
+    test_texts, test_labels = util.load_csv('imdb_test.csv')
+    train_m_path = "imdb_train_matrix.gz"
+    val_m_path = "imdb_val_matrix.gz"
+    test_m_path = "imdb_test_matrix.gz"
 
-    if False:
+    if path.exists(train_m_path):
+        train_matrix = np.loadtxt(train_m_path)
+    else:
         train_matrix = transform_text(train_texts, phoneme_dict)
-        np.savetxt("imdb_train_matrix.gz", train_matrix)
-        val_matrix = transform_text(val_texts, phoneme_dict)
-        np.savetxt("imdb_val_matrix.gz", val_matrix)
-        test_matrix = transform_text(test_texts, phoneme_dict)
-        np.savetxt("imdb_test_matrix.gz", test_matrix)
-    train_matrix = np.loadtxt("imdb_train_matrix.gz")
-    val_matrix = np.loadtxt("imdb_val_matrix.gz")
-    test_matrix = np.loadtxt("imdb_test_matrix.gz")
+        np.savetxt(train_m_path, train_matrix)
 
+    if path.exists(val_m_path):
+        val_matrix = np.loadtxt(val_m_path)
+    else:
+        val_matrix = transform_text(val_texts, phoneme_dict)
+        np.savetxt(val_m_path, val_matrix)
+
+    if path.exists(test_m_path):
+        test_matrix = np.loadtxt(test_m_path)
+    else:
+        test_matrix = transform_text(test_texts, phoneme_dict)
+        np.savetxt(test_m_path, test_matrix)
 
     model = {}
     model["train_matrix"] = train_matrix
